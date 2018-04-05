f7a7eca03e1ef1b6f78fec639f63740a93bf2dc5
pgokul-developer <pgokul.developer@gmail.com>
2018-04-05 13:16:26 +0200

    Create shared data service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
-
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -33,21 +33,29 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = ['My First Goal', 'I want to start a school', 'I want to be an awesome dad and husband'];
+  goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this.changeGoal();
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this.changeGoal();
   }
 
   removeItem(i: number) {
     this.goals.splice(i, 1);
+    this.changeGoal();
+  }
+
+  changeGoal() {
+    this._data.changeGoal(this.goals);
   }
 }
